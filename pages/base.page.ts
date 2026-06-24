0a7f2314da1df2ca6fc381e4c6b81329eae1e88d
hmik2003 <hmik2003@users.noreply.github.com>
2026-06-25 03:21:52 +0500

    chore: remove leftover TypeScript files

--- pages/base.page.ts
+++
@@ -1,9 +0,0 @@
-import { Page } from '@playwright/test';
-
-export class BasePage {
-  constructor(protected readonly page: Page) {}
-
-  async goto(path = '/') {
-    await this.page.goto(path);
-  }
-}
